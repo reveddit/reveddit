4608db7acf51d2edac7b52bdead1effad46cc24b
rhaksw <rdevco.help@gmail.com>
2018-10-25 18:44:14 +0800

    redirect /u/... pages to /user/...

--- src/index.js
+++ src/index.js
@@ -23,23 +23,25 @@ const DefaultLayout = ({component: Component, ...rest}) => {
     )} />
   )
 }
-
-ReactDOM.render(
-  <Provider>
-    <BrowserRouter basename={__dirname}>
-      <React.Fragment>
-        <Switch>
-          <Redirect exact from='/' to='/about' />
-          <DefaultLayout path='/about' component={About} />
-          <DefaultLayout path='/r/:subreddit/comments/:threadID/:junk/:commentID' component={Thread} />
-          <DefaultLayout path='/r/:subreddit/comments/:threadID' component={Thread} />
-          <DefaultLayout path='/r/:subreddit' component={Subreddit} />
-          <DefaultLayout path='/user/:user/:kind' component={User} />
-          <DefaultLayout path='/user/:user' component={User} />
-          <DefaultLayout component={NotFound} />
-        </Switch>
-      </React.Fragment>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('app')
-)
+class App extends React.Component {
+  render() {
+    return (
+      <Provider>
+        <BrowserRouter basename={__dirname}>
+          <Switch>
+            <Redirect exact from='/' to='/about' />
+            <Redirect from='/u/*' to='/user/*' />
+            <DefaultLayout path='/about' component={About} />
+            <DefaultLayout path='/r/:subreddit/comments/:threadID/:junk/:commentID' component={Thread} />
+            <DefaultLayout path='/r/:subreddit/comments/:threadID' component={Thread} />
+            <DefaultLayout path='/r/:subreddit' component={Subreddit} />
+            <DefaultLayout path='/user/:user/:kind' component={User} />
+            <DefaultLayout path='/user/:user' component={User} />
+            <DefaultLayout component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('app'))
