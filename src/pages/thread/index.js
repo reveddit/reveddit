5de7c013bc14d243b9d8ad72d5b307d02069014d
rhaksw <rdevco.help@gmail.com>
2019-04-12 23:38:05 +0800

    set global state on page load for thread pages

--- src/pages/thread/index.js
+++ src/pages/thread/index.js
@@ -34,42 +34,46 @@ class Thread extends React.Component {
     // Get thread from reddit
     const reddit_promise = getPost(subreddit, threadID)
     const pushshift_promise = getPushshiftPost(threadID)
-    Promise.all([reddit_promise, pushshift_promise])
-    .then(values => {
-      const post = values[0]
-      const ps_post = values[1]
-      document.title = post.title
-      const retrievalLatency = ps_post.retrieved_on-ps_post.created_utc
-      if (itemIsRemovedOrDeleted(post)) {
-        if (postIsDeleted(post)) {
-          post.deleted = true
-          post.selftext = ''
-        } else {
-          post.removed = true
-          if (post.is_self) {
-            post.selftext = ps_post.selftext
-          }
-          if (! ps_post.is_crosspostable) {
-            if (retrievalLatency <= AUTOMOD_LATENCY_THRESHOLD) {
-              post.removedby = AUTOMOD_REMOVED
-            } else {
-              post.removedby = UNKNOWN_REMOVED
-            }
+    this.props.global.setStateFromQueryParams(this.props.page_type,
+                    new URLSearchParams(this.props.location.search))
+    .then(result => {
+      Promise.all([reddit_promise, pushshift_promise])
+      .then(values => {
+        const post = values[0]
+        const ps_post = values[1]
+        document.title = post.title
+        const retrievalLatency = ps_post.retrieved_on-ps_post.created_utc
+        if (itemIsRemovedOrDeleted(post)) {
+          if (postIsDeleted(post)) {
+            post.deleted = true
+            post.selftext = ''
           } else {
-            post.removedby = MOD_OR_AUTOMOD_REMOVED
+            post.removed = true
+            if (post.is_self) {
+              post.selftext = ps_post.selftext
+            }
+            if (! ps_post.is_crosspostable) {
+              if (retrievalLatency <= AUTOMOD_LATENCY_THRESHOLD) {
+                post.removedby = AUTOMOD_REMOVED
+              } else {
+                post.removedby = UNKNOWN_REMOVED
+              }
+            } else {
+              post.removedby = MOD_OR_AUTOMOD_REMOVED
+            }
+          }
+        } else {
+          // not-removed posts
+          if ('is_crosspostable' in ps_post && ! ps_post.is_crosspostable) {
+            post.removedby = AUTOMOD_REMOVED_MOD_APPROVED
+          } else {
+            post.removedby = NOT_REMOVED
           }
         }
-      } else {
-        // not-removed posts
-        if ('is_crosspostable' in ps_post && ! ps_post.is_crosspostable) {
-          post.removedby = AUTOMOD_REMOVED_MOD_APPROVED
-        } else {
-          post.removedby = NOT_REMOVED
-        }
-      }
-      this.setState({ post })
+        this.setState({ post })
+      })
+      .catch(this.props.global.setError)
     })
-    .catch(this.props.global.setError)
 
     // Get comment ids from pushshift
     getPushshiftComments(threadID)
