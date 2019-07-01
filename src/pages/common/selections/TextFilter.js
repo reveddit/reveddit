d16369be2414ae4a0d61902ade788db60dcde141
rhaksw <rdevco.help@gmail.com>
2019-07-01 22:15:39 +0800

    debouncing!

--- src/pages/common/selections/TextFilter.js
+++ src/pages/common/selections/TextFilter.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import { connect } from 'state'
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router'
+import { SimpleURLSearchParams } from 'utils'
+import { debounce } from 'lodash'
 
 class TextFilter extends React.Component {
 
+  state = {localKeywords: ""}
+
+  componentDidMount() {
+    const param = new SimpleURLSearchParams(window.location.search).get('keywords') || ''
+    if (param.trim().length) {
+      this.setState({localKeywords: param})
+    }
+    this.updateStateAndURL = debounce(this.props.global.selection_update, 500)
+  }
+
+  changeLocalFast_DelayedGlobalStateUpdate = (e) => {
+    this.setState({localKeywords: e.target.value})
+    this.updateStateAndURL('keywords', e.target.value, this.props)
+  }
+
   render() {
-    const updateStateAndURL = this.props.global.selection_update
     const title = 'Word filter'
-    const textValue = this.props.global.state.keywords
+    const textValue = this.state.localKeywords
     return (
       <div className={`textFilter selection filter ${textValue.trim().length !== 0 ? 'set': ''}`}>
         <div className='title'>{title}</div>
           <input type='text'
             name='keywords' value={textValue} placeholder='keywords' autoFocus='autoFocus'
-            onChange={(e) => updateStateAndURL('keywords', e.target.value, this.props)}
+            onChange={(e) => this.changeLocalFast_DelayedGlobalStateUpdate(e)}
           />
       </div>
     )
