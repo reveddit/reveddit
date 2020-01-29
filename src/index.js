b54b7f1fea640b77df45b51acc169823301b106d
rhaksw <rdevco.help@gmail.com>
2020-01-29 15:14:14 +0800

    standardize path - allow double slash in path - add trailing slash

--- src/index.js
+++ src/index.js
@@ -29,61 +29,72 @@ class App extends React.Component {
       <ApolloProvider client={apolloClient}>
         <Provider>
           <BrowserRouter basename={__dirname}>
-            <Switch>
-              <Redirect exact from='/' to='/about#welcome' />
-              <Redirect from='/u/*' to='/user/*' />
-              <Redirect from='/u/' to='/user/' />
-              <Route path="/user/:user/posts/" component={({ location }) => (
-                  <Redirect
-                    to={{
-                      ...location,
-                      pathname: location.pathname.replace(/\/posts/, '/submitted')
-                    }} /> )}
-              />
-              <Route path="/y/" component={({ location }) => (
-                  <Redirect
-                    to={{
-                      ...location,
-                      pathname: location.pathname.replace(/\/y/, '/user')
-                    }} /> )}
-              />
-              <Route path="/v/" component={({ location }) => (
-                  <Redirect
-                    to={{
-                      ...location,
-                      pathname: location.pathname.replace(/\/v/, '/r')
-                    }} /> )}
-              />
-              <Route path="/api/info/" component={({ location }) => (
-                  <Redirect
-                    to={{
-                      ...location,
-                      pathname: location.pathname.replace(/api\/info/, 'info')
-                    }} /> )}
-              />
-              <DefaultLayout path='/about' component={About} />
-              <DefaultLayout path='/info' page_type='info' component={Info} />
-              <DefaultLayout path='/search' page_type='search' component={Info} />
-              <DefaultLayout path='/random' component={Random} />
-              <DefaultLayout path='/r/:subreddit/comments/:threadID/:urlTitle/:commentID' page_type='thread' component={Thread} />
-              <DefaultLayout path='/r/:subreddit/comments/:threadID/:urlTitle' page_type='thread' component={Thread} />
-              <DefaultLayout path='/r/:subreddit/comments/:threadID' page_type='thread' component={Thread} />
-              <DefaultLayout path='/r/:subreddit/comments/' page_type='subreddit_comments' component={SubredditComments} />
-              <DefaultLayout path='/r/:subreddit/duplicates/:threadID' page_type='duplicate_posts' component={Info} />
-              <DefaultLayout path='/r/:subreddit' page_type='subreddit_posts' component={SubredditPosts} />
-              <DefaultLayout path='/r/' page_type='blank_subreddit' component={BlankSubreddit} />
-              <DefaultLayout path='/comments/' page_type='blank_subreddit_comments' component={BlankSubreddit} is_comments_page={true} />
-              <DefaultLayout path='/domain/all' component={NotFound} />
-              <DefaultLayout path='/domain/:domain' page_type='domain_posts' component={SubredditPosts} />
-              <DefaultLayout path='/user/:userSubreddit/comments/:threadID/:urlTitle/:commentID' page_type='thread' component={Thread} />
-              <DefaultLayout path='/user/:userSubreddit/comments/:threadID/:urlTitle' page_type='thread' component={Thread} />
-              <DefaultLayout path='/user/:userSubreddit/comments/:threadID' page_type='thread' component={Thread} />
-              <DefaultLayout path='/user/:user/:kind' page_type='user' component={User} />
-              <DefaultLayout path='/user/:user' page_type='user' component={User} />
-              <DefaultLayout path='/user/' page_type='blank_user' component={BlankUser} />
-              <DefaultLayout path='/:threadID' page_type='thread' component={Thread} />
-              <DefaultLayout component={NotFound} />
-            </Switch>
+            <Route path='*' render={({location}) => {
+              const path = location.pathname.replace(/\/\/+|([^/])$/g, '$1/')
+              if( path !== location.pathname ) {
+                return <Redirect to={{
+                  ...location,
+                  pathname: path}} />
+              } else {
+                return (
+                  <Switch>
+                    <Redirect exact from='/' to='/about#welcome' />
+                    <Redirect from='/u/*' to='/user/*' />
+                    <Redirect from='/u/' to='/user/' />
+                    <Route path="/user/:user/posts/" component={({ location }) => (
+                        <Redirect
+                          to={{
+                            ...location,
+                            pathname: location.pathname.replace(/\/posts/, '/submitted')
+                          }} /> )}
+                    />
+                    <Route path="/y/" component={({ location }) => (
+                        <Redirect
+                          to={{
+                            ...location,
+                            pathname: location.pathname.replace(/\/y/, '/user')
+                          }} /> )}
+                    />
+                    <Route path="/v/" component={({ location }) => (
+                        <Redirect
+                          to={{
+                            ...location,
+                            pathname: location.pathname.replace(/\/v/, '/r')
+                          }} /> )}
+                    />
+                    <Route path="/api/info/" component={({ location }) => (
+                        <Redirect
+                          to={{
+                            ...location,
+                            pathname: location.pathname.replace(/api\/info/, 'info')
+                          }} /> )}
+                    />
+                    <DefaultLayout path='/about' component={About} />
+                    <DefaultLayout path='/info' page_type='info' component={Info} />
+                    <DefaultLayout path='/search' page_type='search' component={Info} />
+                    <DefaultLayout path='/random' component={Random} />
+                    <DefaultLayout path='/r/:subreddit/comments/:threadID/:urlTitle/:commentID' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/r/:subreddit/comments/:threadID/:urlTitle' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/r/:subreddit/comments/:threadID' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/r/:subreddit/comments/' page_type='subreddit_comments' component={SubredditComments} />
+                    <DefaultLayout path='/r/:subreddit/duplicates/:threadID' page_type='duplicate_posts' component={Info} />
+                    <DefaultLayout path='/r/:subreddit' page_type='subreddit_posts' component={SubredditPosts} />
+                    <DefaultLayout path='/r/' page_type='blank_subreddit' component={BlankSubreddit} />
+                    <DefaultLayout path='/comments/' page_type='blank_subreddit_comments' component={BlankSubreddit} is_comments_page={true} />
+                    <DefaultLayout path='/domain/all' component={NotFound} />
+                    <DefaultLayout path='/domain/:domain' page_type='domain_posts' component={SubredditPosts} />
+                    <DefaultLayout path='/user/:userSubreddit/comments/:threadID/:urlTitle/:commentID' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/user/:userSubreddit/comments/:threadID/:urlTitle' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/user/:userSubreddit/comments/:threadID' page_type='thread' component={Thread} />
+                    <DefaultLayout path='/user/:user/:kind' page_type='user' component={User} />
+                    <DefaultLayout path='/user/:user' page_type='user' component={User} />
+                    <DefaultLayout path='/user/' page_type='blank_user' component={BlankUser} />
+                    <DefaultLayout path='/:threadID' page_type='thread' component={Thread} />
+                    <DefaultLayout component={NotFound} />
+                  </Switch>
+                )
+              }
+            }}/>
           </BrowserRouter>
         </Provider>
       </ApolloProvider>
