8ef8048008e9ff410a4480434920955512d01c94
rhaksw <rdevco.help@gmail.com>
2020-09-28 10:59:22 +0800

    add 'more info' links to action descriptions

--- src/pages/common/RemovedBy.js
+++ src/pages/common/RemovedBy.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {itemIsCollapsed, commentIsMissingInThread,
         isPost, getRemovedWithinText, postRemovedUnknownWithin} from 'utils'
+import {www_reddit} from 'api/reddit'
 
 export const ANTI_EVIL_REMOVED = 'anti_evil_ops'
 export const AUTOMOD_REMOVED = 'automod'
@@ -15,41 +16,53 @@ export const MISSING_IN_THREAD = 'missing'
 export const ORPHANED = 'orphaned'
 export const AUTOMOD_LATENCY_THRESHOLD = 15
 
+const AUTOMOD_LINK = '/wiki/automoderator'
+
 export const REMOVAL_META = {
                  [ANTI_EVIL_REMOVED]: {filter_text: 'reddit anti-evil ops removed',
                                          label: '[removed] by reddit anti-evil ops',
-                                          desc: 'removed by an admin'},
+                                          desc: 'Removed by an admin.',
+                                      morelink: www_reddit+'/9qf5ma'},
         [MOD_OR_AUTOMOD_REMOVED]: {filter_text: 'likely mod removed (maybe automod)',
                                          label: '[removed] likely by mod (maybe automod)',
-                                          desc: '90% chance this was removed by a moderator, 10% chance removed by automod'},
+                                          desc: '90% chance this was removed by a moderator, 10% chance removed by automod.',
+                                      morelink: www_reddit+'/fifhp7'},
                [AUTOMOD_REMOVED]: {filter_text: 'automod removed',
                                          label: '[removed] by automod',
-                                          desc: '100% certain, automod removed'},
+                                          desc: '100% certain, automod removed.',
+                                      morelink: www_reddit+AUTOMOD_LINK},
   [AUTOMOD_REMOVED_MOD_APPROVED]: {filter_text: 'automod removed, manually approved',
                                          label: '[approved] automod removed, manually approved',
-                                          desc: 'This content was initially auto-removed by automoderator and later manually approved by a moderator.'},
+                                          desc: 'This content was initially auto-removed by automoderator and later manually approved by a moderator.',
+                                      morelink: www_reddit+AUTOMOD_LINK},
                [UNKNOWN_REMOVED]: {filter_text: 'unknown if mod or automod removed',
                                          label: '[removed] unknown if by mod or automod',
-                                          desc: 'Cannot say with certainty whether this was removed by a mod or by automod.'},
+                                          desc: 'Cannot say with certainty whether this was removed by a mod or by automod.',
+                                      morelink: www_reddit+'/comments/cve5vl/?comment=ey4mzwq'},
                         [LOCKED]: {filter_text: 'locked',
                                          label: 'locked',
-                                          desc: 'locked'}
+                                          desc: 'locked, replies are not permitted.',
+                                      morelink: www_reddit+'/brgr8i'}
                             }
 
 export const COLLAPSED_META = {filter_text: 'collapsed',
                                      label: 'collapsed',
-                                      desc: 'Comment has a positive score and is collapsed in the thread'}
+                                      desc: 'The comment has a positive score and is collapsed in the thread.',
+                                  morelink: www_reddit+'/e8vl4d'}
 
 export const MISSING_IN_THREAD_META = {filter_text: 'missing in thread',
                                              label: 'missing in thread',
-                                              desc: 'Comment does not appear on the reddit thread unless directly linked'}
+                                              desc: 'The comment does not appear on the reddit thread unless directly linked.',
+                                          morelink: www_reddit+'/gwzbxp'}
 
 export const ORPHANED_META = {filter_text: 'comment parent or link removed',
-                                     desc: 'The thread or the parent of the comment was removed'}
+                                     desc: 'The thread or the parent of the comment was removed.',
+                                 morelink: '/r/TheoryOfReddit/comments/hctddn/reddit_has_a_problem_false_posts_get_removed/fvi50y9/?context=3&add_user=rhaksw.1..new...t1_fvi5di8#t1_fvi50y9'}
 
 export const USER_REMOVED_META = {filter_text: 'user deleted',
                                         label: '[deleted] by user',
-                                         desc: 'user deleted'}
+                                         desc: 'The author of this content deleted it.',
+                                     morelink: www_reddit+'/r/removeddit/comments/ir1oyw/_/g5fgxgl/?context=3#thing_t1_g5fgxgl'}
 
 export const ALL_ACTIONS_META = {
   ...REMOVAL_META,
--- src/pages/modals/ActionHelp.js
+++ src/pages/modals/ActionHelp.js
@@ -2,14 +2,13 @@ import React from 'react'
 import {ALL_ACTIONS_META} from 'pages/common/RemovedBy'
 
 export default () => {
-  console.log(ALL_ACTIONS_META)
   return (
     <>
       {
         Object.entries(ALL_ACTIONS_META).map(([key, meta]) =>
           <div key={key}>
             <h3>{meta.filter_text}</h3>
-            <p>{meta.desc}</p>
+            <p>{meta.desc}{meta.morelink && <> <a target='_blank' href={meta.morelink}>More info</a></>}</p>
           </div>
         )
       }
