df4854216df766cad812151b8a528561c1109f10
rhaksw <rdevco.help@gmail.com>
2020-12-19 12:09:31 +0800

    convert class components to functional

--- src/pages/common/Comment.js
+++ src/pages/common/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { prettyScore, parse,
          PATH_STR_USER, PATH_STR_SUB, convertPathSub, stripRedditLikeDomain,
 } from 'utils'
@@ -14,148 +14,133 @@ import {www_reddit, old_reddit} from 'api/reddit'
 import Flair from './Flair'
 import SubscribersCount from './SubscribersCount'
 
-class Comment extends React.Component {
-  state = {
-    displayBody: true
+const Comment = (props) => {
+  const [displayBody, setDisplayBody] = useState(true)
+  const {t, sort} = props.global.state
+  let classNames = ['comment', 'user']
+  let submitter = ''
+  if (props.is_op) {
+    submitter = ' submitter'+' '
   }
-  toggleDisplayBody() {
-    this.setState({displayBody: ! this.state.displayBody})
+  if (props.removed) {
+    classNames.push('removed')
+  } else if (props.deleted) {
+    classNames.push('deleted')
+  } else {
+    classNames.push('comment-even')
   }
-  getExpandIcon() {
-    if (this.state.displayBody) {
-      return '[–]'
-    } else {
-      return '[+]'
-    }
-  }
+  props.quarantine && classNames.push('quarantine')
+  props.locked && classNames.push('locked')
 
-  render() {
-    const props = this.props
-    const {t, sort} = props.global.state
-    let classNames = ['comment', 'user']
-    let submitter = ''
-    if (props.is_op) {
-      submitter = ' submitter'+' '
-    }
+  let directlink = ''
+  let after_before = '', after = '', before = ''
+  let add_user = ''
+  if (props.prev) {
+    after_before = `after=${props.prev}&`
+    after = props.prev
+  } else if (props.next) {
+    after_before = `before=${props.next}&`
+    before = props.next
+  }
+  if (after_before) {
+    directlink = '?'+after_before+`limit=1&sort=${props.sort}&show=${props.name}&removal_status=all`
     if (props.removed) {
-      classNames.push('removed')
-    } else if (props.deleted) {
-      classNames.push('deleted')
-    } else {
-      classNames.push('comment-even')
+      const addUserParam = new AddUserParam()
+      addUserParam.addItems({
+        author: props.author,
+        ...(props.kind && {kind: props.kind}),
+        ...(sort && {sort: sort}),
+        ...(t && {time: t}),
+        before, after, limit: 1,
+      })
+      add_user = addUserParam.toString()
     }
-    props.quarantine && classNames.push('quarantine')
-    props.locked && classNames.push('locked')
-
-    let directlink = ''
-    let after_before = '', after = '', before = ''
-    let add_user = ''
-    if (props.prev) {
-      after_before = `after=${props.prev}&`
-      after = props.prev
-    } else if (props.next) {
-      after_before = `before=${props.next}&`
-      before = props.next
-    }
-    if (after_before) {
-      directlink = '?'+after_before+`limit=1&sort=${props.sort}&show=${props.name}&removal_status=all`
-      if (props.removed) {
-        const addUserParam = new AddUserParam()
-        addUserParam.addItems({
-          author: props.author,
-          ...(props.kind && {kind: props.kind}),
-          ...(sort && {sort: sort}),
-          ...(t && {time: t}),
-          before, after, limit: 1,
-        })
-        add_user = addUserParam.toString()
-      }
-    }
-    let post_parent_removed = []
-    if (props.parent_removed_label) {
-      post_parent_removed.push('parent '+props.parent_removed_label)
-    }
-    if (props.post_removed_label) {
-      post_parent_removed.push('link '+props.post_removed_label)
-    }
-    const rev_subreddit = PATH_STR_SUB+'/'+props.subreddit
-    const rev_link_permalink = props.link_permalink ?
-      convertPathSub(props.link_permalink.replace(/^https:\/\/[^/]*/,''))
-      : ''
-    return (
-      <div id={props.name} className={classNames.join(' ')} data-fullname={props.name} data-created_utc={props.created_utc}>
-        <div className='comment-head'>
-          <a onClick={() => this.toggleDisplayBody()} className='collapseToggle spaceRight'>{this.getExpandIcon()}</a>
-          <Flair className='link-flair' field='link_flair_text' globalVarName='post_flair' {...props} />
-          <a href={props.url ? stripRedditLikeDomain(props.url) : rev_link_permalink} className='title'
-            >{props.link_title}</a>
-          {'link_author' in props &&
-            <React.Fragment>
-              <span> by </span>
-              <a
-                href={`${PATH_STR_USER}/${props.link_author}`}
-                className='author'>
-                {props.link_author}
-              </a>
-            </React.Fragment>
+  }
+  let post_parent_removed = []
+  if (props.parent_removed_label) {
+    post_parent_removed.push('parent '+props.parent_removed_label)
+  }
+  if (props.post_removed_label) {
+    post_parent_removed.push('link '+props.post_removed_label)
+  }
+  const rev_subreddit = PATH_STR_SUB+'/'+props.subreddit
+  const rev_link_permalink = props.link_permalink ?
+    convertPathSub(props.link_permalink.replace(/^https:\/\/[^/]*/,''))
+    : ''
+  return (
+    <div id={props.name} className={classNames.join(' ')} data-fullname={props.name} data-created_utc={props.created_utc}>
+      <div className='comment-head'>
+        <a onClick={() => setDisplayBody(! displayBody)} className='collapseToggle spaceRight'>{displayBody ? '[–]' : '[+]'}</a>
+        <Flair className='link-flair' field='link_flair_text' globalVarName='post_flair' {...props} />
+        <a href={props.url ? stripRedditLikeDomain(props.url) : rev_link_permalink} className='title'
+          >{props.link_title}</a>
+        {'link_author' in props &&
+          <>
+            <span> by </span>
+            <a
+              href={`${PATH_STR_USER}/${props.link_author}`}
+              className='author'>
+              {props.link_author}
+            </a>
+          </>
+        }
+        {'subreddit' in props &&
+          <>
+            <span> in </span>
+            <a
+              href={rev_subreddit+'/'}
+              className='subreddit-link subreddit'
+              data-subreddit={props.subreddit}
+            >
+              {`/r/${props.subreddit}`}
+            </a> <SubscribersCount {...props}/>
+          </>
+        }
+      </div>
+      <div className='comment-head subhead'>
+        <Author {...props} className='spaceRight'/>
+        <span className='comment-score spaceRight'>{prettyScore(props.score)} point{(props.score !== 1) && 's'}</span>
+        <Time {...props}/>
+        <div>
+          <RemovedBy style={{display:'inline', marginRight: '5px'}} {...props} />
+          {post_parent_removed.length !== 0 &&
+            <RemovedBy removedby={ORPHANED} orphaned_label={post_parent_removed.join(', ')} style={{display:'inline'}}/>
           }
-          {'subreddit' in props &&
-            <React.Fragment>
-              <span> in </span>
-              <a
-                href={rev_subreddit+'/'}
-                className='subreddit-link subreddit'
-                data-subreddit={props.subreddit}
-              >
-                {`/r/${props.subreddit}`}
-              </a> <SubscribersCount {...props}/>
-            </React.Fragment>
-          }
-        </div>
-        <div className='comment-head subhead'>
-          <Author {...props} className='spaceRight'/>
-          <span className='comment-score spaceRight'>{prettyScore(props.score)} point{(props.score !== 1) && 's'}</span>
-          <Time {...props}/>
-          <div>
-            <RemovedBy style={{display:'inline', marginRight: '5px'}} {...props} />
-            {post_parent_removed.length !== 0 &&
-              <RemovedBy removedby={ORPHANED} orphaned_label={post_parent_removed.join(', ')} style={{display:'inline'}}/>
-            }
-          </div>
-        </div>
-        <div className='comment-body-and-links' style={this.state.displayBody ? {} : {display: 'none'}}>
-          <CommentBody {...props}/>
-          <div className='comment-links'>
-            { ! props.deleted &&
-              <>
-                <QuarantinedLabel {...props}/>
-                { props.parent_context ?
-                  <>
-                    <a href={props.parent_context+'?removedby=missing'}>reveddit-parent</a>
-                    <a href={www_reddit+props.parent_context}>reddit-parent</a>
-                    <a href={www_reddit+props.permalink+'?context=1'}>reddit-permalink</a>
-                  </>
-                  :
-                    <a href={props.permalink+'?context=3&'+add_user+'#'+props.name}
-                       onClick={add_user ? hasClickedRemovedUserCommentContext: null}
-                    >context{props.num_replies && `(${props.num_replies})`}</a>
-                }
-                {rev_link_permalink &&
-                  <a href={rev_link_permalink+'?'+add_user}>full comments
-                    {'num_comments' in props && `(${props.num_comments})`}</a>
-                }
-                <a href={old_reddit+props.permalink+'?context=3'}>reddit</a>
-                { directlink && <a href={directlink}>directlink</a>}
-                { ! props.parent_context &&
-                  <MessageMods {...props}/>
-                }
-              </>
-            }
-          </div>
         </div>
       </div>
-    )
-  }
+      <div className='comment-body-and-links' style={displayBody ? {} : {display: 'none'}}>
+        <CommentBody {...props}/>
+        <div className='comment-links'>
+          { ! props.deleted &&
+            <>
+              <QuarantinedLabel {...props}/>
+              { props.parent_context ?
+                <>
+                  <a href={props.parent_context+'?removedby=missing'}>reveddit-parent</a>
+                  <a href={www_reddit+props.parent_context}>reddit-parent</a>
+                  <a href={www_reddit+props.permalink+'?context=1'}>reddit-permalink</a>
+                </>
+                :
+                  <a href={props.permalink+'?context=3&'+add_user+'#'+props.name}
+                     onClick={add_user ? hasClickedRemovedUserCommentContext: null}
+                  >context{props.num_replies && `(${props.num_replies})`}</a>
+              }
+              {rev_link_permalink &&
+                <a href={rev_link_permalink+'?'+add_user}>full comments
+                  {'num_comments' in props && `(${props.num_comments})`}</a>
+              }
+              <a href={old_reddit+props.permalink+'?context=3'}>reddit</a>
+              { directlink && <a href={directlink}>directlink</a>}
+              { ! props.parent_context &&
+                <MessageMods {...props}/>
+              }
+            </>
+          }
+        </div>
+      </div>
+    </div>
+  )
+
 }
 
 export default connect(Comment)
--- src/pages/common/ResultsSummary.js
+++ src/pages/common/ResultsSummary.js
@@ -8,70 +8,67 @@ const posts_page_title = 'user-deleted posts that have no comments are not shown
 const ConditionalWrapper = ({ condition, wrapper, children }) =>
   condition ? wrapper(children) : children
 
-class ResultsSummary extends React.Component {
-  render() {
-    const {num_showing, page_type } = this.props
-    const {before, before_id, items, paginationMeta,
-           itemsSortedByDate = []
-          } = this.props.global.state
-    let {oldestTimestamp, newestTimestamp} = this.props.global.state
-    if (! oldestTimestamp && itemsSortedByDate.length) {
-      oldestTimestamp = itemsSortedByDate[0].created_utc
-      newestTimestamp = itemsSortedByDate[itemsSortedByDate.length-1].created_utc
-    }
-    const youngest_pretty = getPrettyDate(newestTimestamp)
-    const oldest_pretty = getPrettyDate(oldestTimestamp)
+const ResultsSummary = ({global, num_showing, page_type}) => {
+  const {before, before_id, items, paginationMeta,
+         itemsSortedByDate = []
+        } = global.state
+  let {oldestTimestamp, newestTimestamp} = global.state
+  if (! oldestTimestamp && itemsSortedByDate.length) {
+    oldestTimestamp = itemsSortedByDate[0].created_utc
+    newestTimestamp = itemsSortedByDate[itemsSortedByDate.length-1].created_utc
+  }
+  const youngest_pretty = getPrettyDate(newestTimestamp)
+  const oldest_pretty = getPrettyDate(oldestTimestamp)
 
-    let timeFrame = ''
-    let numPagesText = '', totalPagesText = ''
-    if (paginationMeta && paginationMeta.num_pages > 1) {
-      const total = paginationMeta.total_count
-      if (total > items.length) {
-        totalPagesText = ` (${total.toLocaleString()})`
-      }
-      numPagesText = (
-        <div className='non-item text'>page {paginationMeta.page_number} of {paginationMeta.num_pages}</div>
-      )
+  let timeFrame = ''
+  let numPagesText = '', totalPagesText = ''
+  if (paginationMeta && paginationMeta.num_pages > 1) {
+    const total = paginationMeta.total_count
+    if (total > items.length) {
+      totalPagesText = ` (${total.toLocaleString()})`
     }
-
-    // WARNING: a class name & attribute are used by the reveddit extension:
-    // #numItemsLoaded, data-numitemsloaded
-    const numShowingText =
-      <div id='numItemsLoaded' data-numitemsloaded={items.length} title={page_type === 'subreddit_posts' ? posts_page_title : ''}
-         className='non-item text'>
-         <ConditionalWrapper condition={num_showing !== items.length}
-            wrapper={children => <a className='pointer' onClick={() => this.props.global.resetFilters(page_type)}>{children}</a>}
-         >{num_showing.toLocaleString()+' of ' + items.length.toLocaleString()+totalPagesText}</ConditionalWrapper>
-      </div>
-
-    if (before || before_id) {
-      timeFrame =
-        <div>
-          <div className='non-item text'>
-            <Time created_utc={oldestTimestamp} showDate='true' /> — <Time created_utc={newestTimestamp} showDate='true' />
-          </div>
-          <div className='non-item text'>
-            timespan {getPrettyTimeLength(newestTimestamp - oldestTimestamp)}
-          </div>
-        </div>
-    } else if (youngest_pretty !== oldest_pretty) {
-      timeFrame = <div className='non-item text'>
-                    <Time created_utc={newestTimestamp} pretty={youngest_pretty} />
-                    <div>—</div>
-                    <Time created_utc={oldestTimestamp} pretty={oldest_pretty} />
-                  </div>
-    } else {
-      timeFrame = <div className='non-item text'>
-                    since <Time created_utc={oldestTimestamp} pretty={oldest_pretty} />
-                  </div>
-    }
-    return (
-        <div>
-          {timeFrame}
-          {numShowingText}
-          {numPagesText}
-        </div>
+    numPagesText = (
+      <div className='non-item text'>page {paginationMeta.page_number} of {paginationMeta.num_pages}</div>
     )
   }
+
+  // WARNING: a class name & attribute are used by the reveddit extension:
+  // #numItemsLoaded, data-numitemsloaded
+  const numShowingText =
+    <div id='numItemsLoaded' data-numitemsloaded={items.length} title={page_type === 'subreddit_posts' ? posts_page_title : ''}
+       className='non-item text'>
+       <ConditionalWrapper condition={num_showing !== items.length}
+          wrapper={children => <a className='pointer' onClick={() => global.resetFilters(page_type)}>{children}</a>}
+       >{num_showing.toLocaleString()+' of ' + items.length.toLocaleString()+totalPagesText}</ConditionalWrapper>
+    </div>
+
+  if (before || before_id) {
+    timeFrame =
+      <div>
+        <div className='non-item text'>
+          <Time created_utc={oldestTimestamp} showDate='true' /> — <Time created_utc={newestTimestamp} showDate='true' />
+        </div>
+        <div className='non-item text'>
+          timespan {getPrettyTimeLength(newestTimestamp - oldestTimestamp)}
+        </div>
+      </div>
+  } else if (youngest_pretty !== oldest_pretty) {
+    timeFrame = <div className='non-item text'>
+                  <Time created_utc={newestTimestamp} pretty={youngest_pretty} />
+                  <div>—</div>
+                  <Time created_utc={oldestTimestamp} pretty={oldest_pretty} />
+                </div>
+  } else {
+    timeFrame = <div className='non-item text'>
+                  since <Time created_utc={oldestTimestamp} pretty={oldest_pretty} />
+                </div>
+  }
+  return (
+      <div>
+        {timeFrame}
+        {numShowingText}
+        {numPagesText}
+      </div>
+  )
 }
 export default connect(ResultsSummary)
