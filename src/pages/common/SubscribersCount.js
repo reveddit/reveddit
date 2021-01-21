8552b69de76cbced0b7ad63d3e51f81d352bb03a
rhaksw <rdevco.help@gmail.com>
2021-01-21 12:46:00 +0800

    show count when zero

--- src/pages/common/SubscribersCount.js
+++ src/pages/common/SubscribersCount.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { prettyFormatBigNumber } from 'utils'
 
 const SubscribersCount = ({subreddit_subscribers}) => {
-  if (subreddit_subscribers) {
+  if (subreddit_subscribers !== undefined) {
     return '('+prettyFormatBigNumber(subreddit_subscribers)+')'
   }
   return null
