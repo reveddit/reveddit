c00ef768bd32e5e7c5fff665c99f150ec7dbddd6
rhaksw <rdevco.help@gmail.com>
2021-01-22 16:19:32 +0800

    use sort hook

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -9,68 +9,59 @@ import {byScore, byDate, byNumComments, byControversiality,
 import { byNumCrossposts } from 'data_processing/posts'
 import Highlight from 'pages/common/Highlight'
 import Pagination from 'components/Pagination'
+import {useSort} from 'hooks/sort'
 
-export class Info extends React.Component {
+const sortFnMap = {
+  [localSort_types.date]: byDate,
+  [localSort_types.score]: byScore,
+  [localSort_types.controversiality]: byControversiality,
+  [localSort_types.num_comments]: byNumComments,
+  [localSort_types.num_crossposts]: byNumCrossposts,
+  [localSort_types.num_replies]: byNumReplies,
+  [localSort_types.subreddit_subscribers]: bySubredditSubscribers,
+}
+const Info = (props) => {
+  const { page_type, viewableItems, selections, summary, archiveDelayMsg,
+          oldestTimestamp, newestTimestamp, global,
+        } = props
+  const {items, loading, localSort, localSortReverse} = global.state
+  const noItemsFound = items.length === 0 && ! loading && window.location.search !== ''
+  useSort(global, viewableItems, sortFnMap[localSort])
 
-  render() {
-    const { page_type, viewableItems, selections, summary, archiveDelayMsg,
-            oldestTimestamp, newestTimestamp,
-          } = this.props
-    const {items, loading, localSort, localSortReverse} = this.props.global.state
-    const noItemsFound = items.length === 0 && ! loading && window.location.search !== ''
-    const items_sorted = viewableItems
-    if (localSort === localSort_types.date) {
-      items_sorted.sort( reversible(byDate, localSortReverse) )
-    } else if (localSort === localSort_types.score) {
-      items_sorted.sort( reversible(byScore, localSortReverse) )
-    } else if (localSort === localSort_types.controversiality) {
-      items_sorted.sort( reversible(byControversiality, localSortReverse) )
-    } else if (localSort === localSort_types.num_comments) {
-      items_sorted.sort( reversible(byNumComments, localSortReverse) )
-    } else if (localSort === localSort_types.num_crossposts) {
-      items_sorted.sort( reversible(byNumCrossposts, localSortReverse) )
-    } else if (localSort === localSort_types.num_replies) {
-      items_sorted.sort( reversible(byNumReplies, localSortReverse) )
-    } else if (localSort === localSort_types.subreddit_subscribers) {
-      items_sorted.sort( reversible(bySubredditSubscribers, localSortReverse) )
-    }
+  const ids = viewableItems.map(o => o.name)
+  const shareLink = page_type === 'search' ? (
+    <div className="revddit-sharing">
+      <a href={'/info?id='+ids.join(',')} onClick={(e) => copyLink(e, true)}>copy sharelink</a>
+    </div>
+  ) : ''
 
-    const ids = items_sorted.map(o => o.name)
-    const shareLink = page_type === 'search' ? (
-      <div className="revddit-sharing">
-        <a href={'/info?id='+ids.join(',')} onClick={(e) => copyLink(e, true)}>copy sharelink</a>
-      </div>
-    ) : ''
-
-    let pagination = ''
-    if (page_type === 'search') {
-      pagination = <Pagination oldestTimestamp={oldestTimestamp} newestTimestamp={newestTimestamp}
-                               bottom={true}/>
-    }
-
-    return (
-      <div className='infopage'>
-        {shareLink}
-        {selections}
-        {summary}
-        <Highlight/>
-        {archiveDelayMsg}
-        {
-          noItemsFound ?
-          <p>No items found</p> :
-          items_sorted.map(item => {
-            if (item.name.slice(0,2) === 't3') {
-              return <Post key={item.name} {...item} page_type={page_type} />
-            } else {
-              return <Comment key={item.name} {...item} page_type={page_type} />
-            }
-          })
-        }
-        {pagination}
-      </div>
-    )
-
+  let pagination = ''
+  if (page_type === 'search') {
+    pagination = <Pagination oldestTimestamp={oldestTimestamp} newestTimestamp={newestTimestamp}
+                             bottom={true}/>
   }
+
+  return (
+    <div className='infopage'>
+      {shareLink}
+      {selections}
+      {summary}
+      <Highlight/>
+      {archiveDelayMsg}
+      {
+        noItemsFound ?
+        <p>No items found</p> :
+        viewableItems.map(item => {
+          if (item.name.slice(0,2) === 't3') {
+            return <Post key={item.name} {...item} page_type={page_type} />
+          } else {
+            return <Comment key={item.name} {...item} page_type={page_type} />
+          }
+        })
+      }
+      {pagination}
+    </div>
+  )
 }
 
 export default connect(withFetch(Info))
