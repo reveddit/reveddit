4188555c564fd56f3b8ac39130beb12f9ea7d00d
rhaksw <rdevco.help@gmail.com>
2021-02-16 14:52:56 +0800

    useMemo on items loop

--- src/pages/common/selections/CategoryFilter.js
+++ src/pages/common/selections/CategoryFilter.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import { connect } from 'state'
 import { Selection } from './SelectionBase'
 
-class CategoryFilter extends React.Component {
-
-  render() {
+const CategoryFilter = connect((props) => {
+  const { global, type, title, page_type } = props
+  let unique_field = type
+  if (props.unique_field) {
+    unique_field = props.unique_field
+  }
+  const {items, add_user, add_user_on_page_load} = global.state
+  const computeCountAndSortMeta = () => {
     const category_visible_counts = {}
     const category_counts = {}
     const category_unique_to_displayValue = {}
-
-    const { type, title, page_type } = this.props
-    let unique_field = type
-    if (this.props.unique_field) {
-      unique_field = this.props.unique_field
-    }
-    const items = this.props.global.state.items
     items.forEach(item => {
       const unique_value = item[unique_field]
       category_visible_counts[unique_value] = 0
@@ -25,45 +23,56 @@ class CategoryFilter extends React.Component {
         category_counts[unique_value] = 1
       }
     })
-    this.props.visibleItemsWithoutCategoryFilter.forEach(item => {
+    props.visibleItemsWithoutCategoryFilter.forEach(item => {
       category_visible_counts[item[unique_field]] += 1
     })
     const category_ordered = Object.keys(category_visible_counts).sort((a,b) => {
       let alpha = a.toLowerCase() < b.toLowerCase() ? -1 : 1
       return (category_visible_counts[b] - category_visible_counts[a]) || alpha
     })
-    let categoryFilter = this.props.global.state['categoryFilter_'+type]
-    if (! categoryFilter) {
-      categoryFilter = 'all'
-    }
-    const updateStateAndURL = this.props.global.categoryFilter_update
 
-    return (
-      <Selection className='categoryFilter' isFilter={true} isSet={categoryFilter !== 'all'} title={title}>
-        <select value={categoryFilter}
-          onChange={(e) => updateStateAndURL(type, e.target.value, page_type )}>
-          <option key='all' value='all'>all</option>
-          {
-            category_ordered.map(category => {
-              let displayValue = category_unique_to_displayValue[category]
-              if (! displayValue) {
-                //handle when [deleted] author gets filled in
-                return null
-              }
-              if (displayValue.length > 30) {
-                displayValue = displayValue.substr(0, 30)+'...'
-              }
-              return (
-                <option key={category} value={category}>
-                  ({`${category_visible_counts[category]} / ${category_counts[category]}`}) {displayValue}
-                </option>
-              )
-            })
-          }
-        </select>
-      </Selection>
-    )
+    return {category_unique_to_displayValue,
+            category_counts,
+            category_visible_counts,
+            category_ordered,
+           }
   }
-}
+  const {category_unique_to_displayValue,
+         category_counts,
+         category_visible_counts,
+         category_ordered,
+       } = useMemo(computeCountAndSortMeta, [items.length, add_user, add_user_on_page_load])
+  let categoryFilter = global.state['categoryFilter_'+type]
+  if (! categoryFilter) {
+    categoryFilter = 'all'
+  }
+  const updateStateAndURL = global.categoryFilter_update
 
-export default connect(CategoryFilter)
+  return (
+    <Selection className='categoryFilter' isFilter={true} isSet={categoryFilter !== 'all'} title={title}>
+      <select value={categoryFilter}
+        onChange={(e) => updateStateAndURL(type, e.target.value, page_type )}>
+        <option key='all' value='all'>all</option>
+        {
+          category_ordered.map(category => {
+            let displayValue = category_unique_to_displayValue[category]
+            if (! displayValue) {
+              //handle when [deleted] author gets filled in
+              return null
+            }
+            if (displayValue.length > 30) {
+              displayValue = displayValue.substr(0, 30)+'...'
+            }
+            return (
+              <option key={category} value={category}>
+                ({`${category_visible_counts[category]} / ${category_counts[category]}`}) {displayValue}
+              </option>
+            )
+          })
+        }
+      </select>
+    </Selection>
+  )
+})
+
+export default CategoryFilter
