7d4ec42952f98aebc9f3a1f32947bd6bc6d196fb
rhaksw <rdevco.help@gmail.com>
2021-02-27 18:27:49 +0800

    separate chunk for each vendor package - should save users bandwidth in the long term

--- webpack.config.js
+++ webpack.config.js
@@ -109,6 +109,27 @@ module.exports = async (env, argv) => {
       }),
     ],
     optimization: {
+      // from https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+      // and  https://stackoverflow.com/a/52961891/2636364
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
       minimize: IS_PRODUCTION,
       minimizer: [
         new TerserPlugin({
