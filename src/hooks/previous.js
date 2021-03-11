c493c2026f37fbed5c20a938cf5586297056927e
rhaksw <rdevco.help@gmail.com>
2021-03-11 08:28:23 +0800

    missing file

---
+++ src/hooks/previous.js
@@ -0,0 +1,9 @@
+import React, {useRef, useEffect} from 'react'
+
+export const usePrevious = (value) => {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
