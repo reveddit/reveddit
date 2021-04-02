f4e3d58cf7b6080122128e249e7cb2030b867768
rhaksw <rdevco.help@gmail.com>
2021-04-02 16:08:31 +0800

    show inline images in self-posts

--- src/pages/common/Post.js
+++ src/pages/common/Post.js
@@ -16,6 +16,7 @@ import SubscribersCount from './SubscribersCount'
 import {getAddUserParamString} from './Comment'
 import {AuthorFocus} from 'pages/thread/Comment'
 import {get_userPageSortAndTime} from 'data_processing/FindCommentViaAuthors'
+
 const max_selftext_length = 100
 
 const decodeHtml = (html) => {
@@ -24,6 +25,7 @@ const decodeHtml = (html) => {
     return txt.value || html
 }
 const clear = <div className='clearBoth' style={{flexBasis:'100%', height: '0'}}></div>
+
 const Post = connect((props) => {
   const {
     global, rev_position, page_type,
@@ -41,7 +43,7 @@ const Post = connect((props) => {
     manuallyHiddenSelftext: false,
   })
   const [focusedMediaKey, setFocusedMediaKey] = useState(null)
-  const focusedImageList = media_metadata ? media_metadata[focusedMediaKey]?.p : undefined
+  const focusedImageList = getImageList({media_metadata, id: focusedMediaKey})
   const {displayFullSelftext, manuallyDisplayedSelftext, manuallyHiddenSelftext} = selftextMeta
   const [localLoading, setLocalLoading] = useState(false)
   const loading = localLoading || globalLoading
@@ -176,7 +178,7 @@ const Post = connect((props) => {
                 {snippet_is_set &&
                   <a className='collapseToggle' onClick={hideFullSelftext} style={{float:'left',marginRight:'10px'}}>[–]</a>
                 }
-                <div dangerouslySetInnerHTML={{ __html: parse(selftext)}}/>
+                <Selftext {...{selftext, media_metadata}}/>
                 {snippet_is_set &&
                   <p><a className='collapseToggle' onClick={hideFullSelftext} style={{float:'left',marginRight:'10px'}}>[–] view less</a></p>
                 }
@@ -200,13 +202,70 @@ const Post = connect((props) => {
   )
 })
 
-const Image = ({x, y, u, onClick}) => {
-  return <img className='pointer' width={x} height={y} src={replaceAmpGTLT(u)} onClick={onClick} />
+const redditPreview_regexpString_base = 'https://preview\\.redd\\.it/'
+const redditPreview_regexpString_withParen = redditPreview_regexpString_base+'[^\\s]+'
+const redditPreview_regexpString_noParen = redditPreview_regexpString_base+'([^.]+)\\.[^)\\s]+'
+const splitOnPreview_regexp = new RegExp('((?:\\[[^\\]]*\\]\\()?'+redditPreview_regexpString_withParen+')')
+const maxWidthInsideSelftextBox = 835
+const calculateMaxImageSizeForScreen = () => window.innerWidth - 40
+const Selftext = ({selftext, media_metadata}) => {
+  if (media_metadata) {
+    return <SelftextInParts {...{selftext, media_metadata}} />
+  } else {
+    return <SelftextPart selftext={selftext} />
+  }
 }
 
-const BestImage = ({list, onClick}) => {
+const SelftextPart = ({selftext}) => <div dangerouslySetInnerHTML={{__html: parse(selftext)}}/>
+
+const SelftextInParts = ({selftext, media_metadata}) => {
+  const selftextParts = selftext.split(splitOnPreview_regexp)
+  const result = []
+  const maxImageSizeForScreen = calculateMaxImageSizeForScreen()
+  const maxWidth = maxImageSizeForScreen < maxWidth ? maxImageSizeForScreen : maxWidthInsideSelftextBox
+  for (const [i, part] of selftextParts.entries()) {
+    const match = part.match(new RegExp('(?:\\[([^\\]]*)\\]\\()?('+redditPreview_regexpString_noParen+')'))
+    if (match) {
+      const [ , caption, url, id] = match
+      const list = getImageList({media_metadata, id})
+      if (list) {
+        const marginTop = caption ? '5px' : ''
+        result.push(
+          <div key={i} style={{textAlign:'center', marginTop}}>
+            {caption && <h4>{caption}</h4>}
+            <BestImage key={i} list={list} maxWidth={maxWidth}/>
+          </div>
+        )
+      }
+      continue
+    }
+    result.push(<SelftextPart key={i} selftext={part}/>)
+  }
+  return result
+}
+
+
+const getImageList = ({media_metadata, id}) => {
+  if (media_metadata && id) {
+    const imageData = media_metadata[id]
+    if (imageData) {
+      const list = imageData.p
+      if (list && imageData.s) {
+        list.push(imageData.s)
+      }
+      return list
+    }
+  }
+  return undefined
+}
+
+const Image = ({x, y, u, onClick}) => {
+  return <img className={onClick ? 'pointer' : ''} width={x} height={y} src={replaceAmpGTLT(u)} onClick={onClick} />
+}
+
+const BestImage = ({list, onClick, maxWidth = calculateMaxImageSizeForScreen()}) => {
   for (const preview of list.slice().reverse()) {
-    if (preview.x < window.innerWidth - 40) {
+    if (preview.x < maxWidth) {
       return <Image {...preview} onClick={onClick} />
     }
   }
