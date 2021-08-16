3f16c71e219ec945dd037cef10dd461795338254
rhaksw <rdevco.help@gmail.com>
2021-08-16 11:20:43 +0800

    to verify missing comment, get as many as possible

--- src/pages/common/Comment.js
+++ src/pages/common/Comment.js
@@ -120,7 +120,7 @@ const Comment = (props) => {
               { parent_context ?
                 <>
                   <a href={parent_context+'?removedby=missing'}>reveddit-parent</a>
-                  <NewWindowLink reddit={parent_context}>reddit-parent</NewWindowLink>
+                  <NewWindowLink reddit={parent_context+'?limit=500'}>reddit-parent</NewWindowLink>
                   <NewWindowLink reddit={permalink+'?context=1'}>reddit-permalink</NewWindowLink>
                 </>
                 :
