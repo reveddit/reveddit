81b32fcb76f84d302d57074aad7b1399ff818b3a
rhaksw <rdevco.help@gmail.com>
2021-09-04 14:29:16 +0800

    link to code

--- src/pages/common/selections/UpvoteRemovalRateHistory.js
+++ src/pages/common/selections/UpvoteRemovalRateHistory.js
@@ -7,7 +7,7 @@ import { prettyFormatBigNumber, SimpleURLSearchParams, ifNumParseInt,
 } from 'utils'
 import { Fetch } from 'hooks/fetch'
 import { Selection } from './SelectionBase'
-import { QuestionMarkModal, Help } from 'components/Misc'
+import { QuestionMarkModal, Help, NewWindowLink } from 'components/Misc'
 import { getAggregationsURL,
   numGraphPointsParamKey, sortByParamKey, contentTypeParamKey, aggregationPeriodParams,
   agg_defaults_for_page,
@@ -18,7 +18,7 @@ const urr_title = 'Karma Removal Rate'
 const own_page_text = 'items preview page'
 export const urr_help = <Help title={urr_title} content={
   <>
-    <p>This shows highly upvoted removed content for any subreddit.</p>
+    <p>This shows highly upvoted removed content for any subreddit. Code that generates the data is available here: <NewWindowLink href="https://github.com/reveddit/ragger">github.com/reveddit/ragger</NewWindowLink></p>
     <p><b>How is it calculated?</b></p>
     <p>The rate shown is the percentage of karma removed in periods of either 1,000 comments or 1,000 posts over time. Each item represents the summed score of removed items in that period divided by the summed score of all items for that period. The comment or post next to the rate is a preview of the removed item with the highest score in the period.</p>
     <p>For example, if 1,000 items have a combined score of 20,000 and the removed items have a combined score of 10,000, then the removal rate for that period is 50%, and the previewed item may have had a high score such as 7,000.</p>
