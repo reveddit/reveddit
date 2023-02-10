fec6d6b4fd79ae63c1f8939382ac125a0521c672
rhaksw <rdevco.help@gmail.com>
2023-02-10 16:56:46 +0800

    add tooltip to subscriber count

--- src/pages/common/SubscribersCount.js
+++ src/pages/common/SubscribersCount.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { prettyFormatBigNumber } from 'utils'
 
 const SubscribersCount = ({subreddit_subscribers}) => {
+  const summary_count = prettyFormatBigNumber(subreddit_subscribers)
   if (subreddit_subscribers !== undefined) {
-    return '('+prettyFormatBigNumber(subreddit_subscribers)+')'
+    return <span title={summary_count+' subscribers'}>({summary_count})</span>
   }
   return null
 }
