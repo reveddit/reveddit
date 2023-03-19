dc2191ce3dc8f6ca209a99836ec3adf35f0af01f
jon99-au <jon99-au@users.noreply.github.com>
2023-03-19 19:43:55 +1100

    replace existing shortcut with v2, limit to the single shortcut

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -31,8 +31,7 @@ export const is_iOS = (
   || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 )
 
-export const iOS_shortcut_link = <a href='https://www.icloud.com/shortcuts/62bc7570613c42cb8b851fad264136df'>iOS shortcut</a>
-export const iOS_old_reddit_shortcut_link = <a href='https://www.icloud.com/shortcuts/58cadd9977f442a7a9ab71626ee2300f'>iOS shortcut Old Reddit </a>
+export const iOS_shortcut_link = <a href='https://www.icloud.com/shortcuts/d18cb266c9b6494faf8aef38ab60c607'>iOS shortcut</a>
 
 let browserExtensionImage = ''
 if (isChrome) {
--- src/pages/common/Highlight.js
+++ src/pages/common/Highlight.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {ext_urls} from 'utils'
-import {Tip, ExtensionLink, is_iOS, iOS_shortcut_link, iOS_old_reddit_shortcut_link} from 'components/Misc'
+import {Tip, ExtensionLink, is_iOS, iOS_shortcut_link} from 'components/Misc'
 
 export default ({showMobile = false}) => {
   return (
     <div className={`note highlight real-time ${showMobile ? '' : 'desktop-only'}`}>
       {is_iOS ? <Tip>{iOS_shortcut_link}</Tip> : <></>}
-      {is_iOS ? <Tip>{iOS_old_reddit_shortcut_link}</Tip> : <></>}
       <Tip><ExtensionLink/> can notify you when your content is removed.</Tip>
     </div>
   )
