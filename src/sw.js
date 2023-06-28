503fc5530410a49f122da77da1bc0fa7ff6acbd7
rhaksw <rdevco.help@gmail.com>
2023-06-28 12:09:47 +0800

    remove conflicting entry per error: "Uncaught add-to-cache-list-conflicting-entries: add-to-cache-list-conflicting-entries :: [{"firstEntry":"https://dev.reveddit.com/index.html","secondEntry":"https://dev.reveddit.com/index.html?__WB_REVISION__=88852b1adae36f31dd95a1816d46ac47"}]"

--- src/sw.js
+++ src/sw.js
@@ -12,7 +12,7 @@ clientsClaim();
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-precacheAndRoute([{url: '/index.html'}].concat(self.__WB_MANIFEST));
+precacheAndRoute(self.__WB_MANIFEST);
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
