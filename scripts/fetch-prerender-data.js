95d0facd1461b06784dc77ea15c2ee1bb2832437
rhaksw <rdevco.help@gmail.com>
2026-01-08 13:54:42 -0500

    Prerender

---
+++ scripts/fetch-prerender-data.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+
+// Match webpack.config.js logic:
+// - Production: https://api.reveddit.com/short/what-people-say/
+// - Development: http://localhost:5000/what-people-say/
+const IS_NETLIFY = process.env.NETLIFY === 'true'
+const IS_PRODUCTION = IS_NETLIFY || process.env.NODE_ENV === 'production'
+
+const API_URL = IS_PRODUCTION
+    ? 'https://api.reveddit.com/short/what-people-say/?c=prerender'
+    : 'http://localhost:5000/what-people-say/?c=prerender'
+
+const OUTPUT_PATH = './src/data/what-people-say.json'
+
+console.log(`Fetching what-people-say data from ${API_URL}...`)
+
+const protocol = API_URL.startsWith('https') ? https : http
+
+protocol.get(API_URL, (res) => {
+    if (res.statusCode !== 200) {
+        console.error(`❌ API returned status ${res.statusCode}`)
+        process.exit(1)
+    }
+
+    let data = ''
+    res.on('data', chunk => data += chunk)
+    res.on('end', () => {
+        try {
+            // Validate JSON
+            JSON.parse(data)
+
+            fs.mkdirSync('./src/data', { recursive: true })
+            fs.writeFileSync(OUTPUT_PATH, data)
+            console.log('✅ Saved what-people-say.json')
+        } catch (err) {
+            console.error('❌ Invalid JSON response:', err.message)
+            process.exit(1)
+        }
+    })
+}).on('error', (err) => {
+    console.error('❌ Failed to fetch:', err.message)
+    process.exit(1)
+})
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { Provider } from 'unstated'
-import DefaultLayout, {pageTypes} from 'pages/DefaultLayout'
+import DefaultLayout, { pageTypes } from 'pages/DefaultLayout'
 import ErrorBoundary from 'components/ErrorBoundary'
-import {PATH_STR_SUB, PATH_STR_USER,
-        PATHS_ALT_SUB, PATHS_ALT_USER,
-        SimpleURLSearchParams,
+import {
+  PATH_STR_SUB, PATH_STR_USER,
+  PATHS_ALT_SUB, PATHS_ALT_USER,
+  SimpleURLSearchParams,
 } from 'utils'
-import {urlParamKeys_textFilters} from 'state'
-import {ExtensionRedirect} from 'components/Misc'
-import {old_reddit} from 'api/reddit'
+import { urlParamKeys_textFilters } from 'state'
+import { ExtensionRedirect } from 'components/Misc'
+import { old_reddit } from 'api/reddit'
 
 const PARAMKEYS_DONT_REMOVE_BACKSLASH = new Set(Object.values(urlParamKeys_textFilters))
 
@@ -43,84 +44,84 @@ const ThreadRedirect = lazy(() => import('pages/thread/redirect'))
 const NotFound = lazy(() => import('pages/404'))
 const Random = lazy(() => import('pages/about/random'))
 
-const RouteRedirectWithParams = ({path, search, replace}) =>
+const RouteRedirectWithParams = ({ path, search, replace }) =>
   <Route path={path} component={({ location }) => (
-      <Redirect
-        to={{
-          ...location,
-          pathname: location.pathname.replace(search, replace)
-        }} /> )}
+    <Redirect
+      to={{
+        ...location,
+        pathname: location.pathname.replace(search, replace)
+      }} />)}
   />
 
 const getAltRoutes = (alt_paths, replace_path) => {
   return alt_paths.map(altPath =>
-    <RouteRedirectWithParams key={altPath} path={`${altPath}/`} search={new RegExp(altPath)} replace={replace_path}/>
+    <RouteRedirectWithParams key={altPath} path={`${altPath}/`} search={new RegExp(altPath)} replace={replace_path} />
   )
 }
 
-const profile_search = new RegExp('^'+PATH_STR_USER + '/')
+const profile_search = new RegExp('^' + PATH_STR_USER + '/')
 const profile_replace = PATH_STR_SUB + '/u_'
 
 
 const routes = (
-<Switch>
-  <Redirect exact from='/about' to='/' />
-  <RouteRedirectWithParams path={PATH_STR_USER+'/:user/posts/'} search={/\/posts/} replace='/submitted'/>
-  <RouteRedirectWithParams path={PATH_STR_SUB+'/:subreddit/controversial/'} search={/\/controversial/} replace='/history'/>
-  <RouteRedirectWithParams path={PATH_STR_SUB+'/:subreddit/top/'} search={/\/top/} replace='/history'/>
-  {getAltRoutes(PATHS_ALT_USER, PATH_STR_USER)}
-  {getAltRoutes(PATHS_ALT_SUB, PATH_STR_SUB)}
-  <RouteRedirectWithParams path='/api/info/' search={/\/api\/info/} replace='/info'/>
-  <RouteRedirectWithParams path='/gallery/' search={/\/gallery/} replace=''/>
-  <RouteRedirectWithParams path='/about/f.a.q.' search='/f.a.q.' replace='/faq'/>
-  <RouteRedirectWithParams path='/submit' search='/submit' replace='/info'/>
-  <DefaultLayout exact path='/' component={About} title='About reveddit'/>
-  <DefaultLayout path='/about/faq' component={About_faq} title='Frequently Asked Questions'/>
-  <DefaultLayout path='/about/contact' component={About_contact} title='Contact'/>
-  <DefaultLayout path='/about/donate' component={About_donate} title='Donate'/>
-  <DefaultLayout path='/add-ons/direct' component={ExtensionRedirect} />
-  <DefaultLayout path='/add-ons/linker' component={ExtensionRedirect} extCode='linker' />
-  <DefaultLayout path='/add-ons/real-time' component={ExtensionRedirect} extCode='rt' />
-  <DefaultLayout path='/add-ons' component={AddOns} />
-  <DefaultLayout path='/info' page_type='info' component={Info} />
-  <DefaultLayout path='/search' page_type='search' component={Info} />
-  <DefaultLayout path='/random' component={Random} title='Find Random User'/>
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/x'} component={Random} title='Find Random User'/>
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/history'} page_type={pageTypes.aggregations} component={Aggregations} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/missing-comments'} page_type={pageTypes.missing_comments} component={SubredditComments} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/about/sticky'} page_type='sticky' component={SubredditSticky} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/comments/:threadID/:urlTitle/:commentID'} page_type='thread' component={Thread} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/comments/:threadID/:urlTitle'} page_type='thread' component={Thread} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/comments/:threadID'} page_type='thread' component={Thread} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/comments/'} page_type='subreddit_comments' component={SubredditComments} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit/duplicates/:threadID'} page_type='duplicate_posts' component={Info} />
-  <DefaultLayout path={PATH_STR_SUB+'/:subreddit'} page_type={pageTypes.subreddit_posts} component={SubredditPosts} />
-  <DefaultLayout path={PATH_STR_SUB+'/'} page_type='blank_subreddit' component={BlankSubreddit} />
-  <DefaultLayout path='/comments/' page_type='blank_subreddit_comments' component={BlankSubreddit} is_comments_page={true} />
-  <DefaultLayout path='/domain/all' component={NotFound} />
-  <DefaultLayout path='/domain/:domain' page_type='domain_posts' component={SubredditPosts} />
-  <RouteRedirectWithParams path={PATH_STR_USER+'/:user/comments/:threadID'} search={profile_search} replace={profile_replace} />
-  <RouteRedirectWithParams path={PATH_STR_USER+'/:user/duplicates/:threadID'} search={profile_search} replace={profile_replace} />
-  <DefaultLayout path={PATH_STR_USER+'/:user/:kind'} page_type='user' component={User} />
-  <DefaultLayout path={PATH_STR_USER+'/:user'} page_type='user' component={User} />
-  <DefaultLayout path={PATH_STR_USER+'/'} page_type='blank_user' component={BlankUser} />
-  <DefaultLayout path='/:threadID' page_type='thread' component={ThreadRedirect} />
-  <DefaultLayout component={NotFound} />
-</Switch>)
+  <Switch>
+    <Redirect exact from='/about' to='/' />
+    <RouteRedirectWithParams path={PATH_STR_USER + '/:user/posts/'} search={/\/posts/} replace='/submitted' />
+    <RouteRedirectWithParams path={PATH_STR_SUB + '/:subreddit/controversial/'} search={/\/controversial/} replace='/history' />
+    <RouteRedirectWithParams path={PATH_STR_SUB + '/:subreddit/top/'} search={/\/top/} replace='/history' />
+    {getAltRoutes(PATHS_ALT_USER, PATH_STR_USER)}
+    {getAltRoutes(PATHS_ALT_SUB, PATH_STR_SUB)}
+    <RouteRedirectWithParams path='/api/info/' search={/\/api\/info/} replace='/info' />
+    <RouteRedirectWithParams path='/gallery/' search={/\/gallery/} replace='' />
+    <RouteRedirectWithParams path='/about/f.a.q.' search='/f.a.q.' replace='/faq' />
+    <RouteRedirectWithParams path='/submit' search='/submit' replace='/info' />
+    <DefaultLayout exact path='/' component={About} title='About reveddit' />
+    <DefaultLayout path='/about/faq' component={About_faq} title='Frequently Asked Questions' />
+    <DefaultLayout path='/about/contact' component={About_contact} title='Contact' />
+    <DefaultLayout path='/about/donate' component={About_donate} title='Donate' />
+    <DefaultLayout path='/add-ons/direct' component={ExtensionRedirect} />
+    <DefaultLayout path='/add-ons/linker' component={ExtensionRedirect} extCode='linker' />
+    <DefaultLayout path='/add-ons/real-time' component={ExtensionRedirect} extCode='rt' />
+    <DefaultLayout path='/add-ons' component={AddOns} />
+    <DefaultLayout path='/info' page_type='info' component={Info} />
+    <DefaultLayout path='/search' page_type='search' component={Info} />
+    <DefaultLayout path='/random' component={Random} title='Find Random User' />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/x'} component={Random} title='Find Random User' />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/history'} page_type={pageTypes.aggregations} component={Aggregations} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/missing-comments'} page_type={pageTypes.missing_comments} component={SubredditComments} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/about/sticky'} page_type='sticky' component={SubredditSticky} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/comments/:threadID/:urlTitle/:commentID'} page_type='thread' component={Thread} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/comments/:threadID/:urlTitle'} page_type='thread' component={Thread} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/comments/:threadID'} page_type='thread' component={Thread} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/comments/'} page_type='subreddit_comments' component={SubredditComments} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit/duplicates/:threadID'} page_type='duplicate_posts' component={Info} />
+    <DefaultLayout path={PATH_STR_SUB + '/:subreddit'} page_type={pageTypes.subreddit_posts} component={SubredditPosts} />
+    <DefaultLayout path={PATH_STR_SUB + '/'} page_type='blank_subreddit' component={BlankSubreddit} />
+    <DefaultLayout path='/comments/' page_type='blank_subreddit_comments' component={BlankSubreddit} is_comments_page={true} />
+    <DefaultLayout path='/domain/all' component={NotFound} />
+    <DefaultLayout path='/domain/:domain' page_type='domain_posts' component={SubredditPosts} />
+    <RouteRedirectWithParams path={PATH_STR_USER + '/:user/comments/:threadID'} search={profile_search} replace={profile_replace} />
+    <RouteRedirectWithParams path={PATH_STR_USER + '/:user/duplicates/:threadID'} search={profile_search} replace={profile_replace} />
+    <DefaultLayout path={PATH_STR_USER + '/:user/:kind'} page_type='user' component={User} />
+    <DefaultLayout path={PATH_STR_USER + '/:user'} page_type='user' component={User} />
+    <DefaultLayout path={PATH_STR_USER + '/'} page_type='blank_user' component={BlankUser} />
+    <DefaultLayout path='/:threadID' page_type='thread' component={ThreadRedirect} />
+    <DefaultLayout component={NotFound} />
+  </Switch>)
 
 class App extends React.Component {
   render() {
     return (
       <Provider>
         <BrowserRouter basename={__dirname}>
-          <Route path='*' render={({location}) => {
+          <Route path='*' render={({ location }) => {
             if (location.pathname.match(/^\/+https?:\/\//)) {
               return (
                 <Redirect
                   to={{
                     ...location,
                     pathname: '/info/',
-                    search: '?url='+encodeURIComponent(location.pathname.replace(/^\/+/,'')+location.search),
+                    search: '?url=' + encodeURIComponent(location.pathname.replace(/^\/+/, '') + location.search),
                   }} />
               )
             }
@@ -128,7 +129,7 @@ class App extends React.Component {
             //also convert paths ending in /.compact to /
             let pathname = location.pathname
               .replace(/\/\/+|([^/])$|\/\.compact$/g, '$1/')
-              .replace(/\\/g,'')
+              .replace(/\\/g, '')
 
             //new reddit's fancy editor has a bug, when you write a URL w/out formatting and switch to markdown, it inserts a \ before all _
             //so, remove \ from add_user param (don't want to remove \ from text filter params like keywords or flair)
@@ -145,14 +146,15 @@ class App extends React.Component {
                   return null
                 }
                 // remove ! from usernames. bot inserts them to avoid automod matches on usernames
-                pathname = pathname.replace(/!/g,'')
+                pathname = pathname.replace(/!/g, '')
               }
             }
             if (pathname !== location.pathname || search !== location.search) {
               return <Redirect to={{
                 ...location,
                 search,
-                pathname}} />
+                pathname
+              }} />
             } else {
               return (
                 <ErrorBoundary>
@@ -162,10 +164,15 @@ class App extends React.Component {
                 </ErrorBoundary>
               )
             }
-          }}/>
+          }} />
         </BrowserRouter>
       </Provider>
     )
   }
 }
-ReactDOM.render(<App />, document.getElementById('app'))
+const rootElement = document.getElementById('app')
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement)
+} else {
+  ReactDOM.render(<App />, rootElement)
+}
